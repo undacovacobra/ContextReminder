ec47c5718e7e9d94a026f8bba59cad2504502fd9
undacovacobra <thetravisj1989@gmail.com>
2026-08-29 21:04:58 -0400

    chore: bump app to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.contextreminder.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     buildFeatures {
