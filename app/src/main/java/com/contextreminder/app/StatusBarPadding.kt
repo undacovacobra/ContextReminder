f88ef3742b694de7ef9f5f54f98d6d779b52a5f1
undacovacobra <thetravisj1989@gmail.com>
2026-08-29 21:09:16 -0400

    fix: mark status bar padding composable

--- app/src/main/java/com/contextreminder/app/StatusBarPadding.kt
+++ app/src/main/java/com/contextreminder/app/StatusBarPadding.kt
@@ -3,6 +3,8 @@ package com.contextreminder.app
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@Composable
 fun Modifier.statusBarsPadding(): Modifier = windowInsetsPadding(WindowInsets.statusBars)
